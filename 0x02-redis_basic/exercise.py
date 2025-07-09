bec914cd1a3cd1ac4f7421ad80044d8e9503a506
Muse Semu <muses@act.com.et>
2025-07-09 10:30:00 +0300

    Updating the script to enable it store lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,6 +13,19 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+def call_history(method: Callable) -> Callable:
+    """Decorator to store input and output history of a method in Redis lists"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper function that stores inputs/outputs and calls original method"""
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+        self._redis.rpush(input_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, output)
+        return output
+    return wrapper
+
 class Cache:
     def __init__(self):
         """Initialize Redis client and flush database"""
@@ -20,6 +33,7 @@ class Cache:
         self._redis.flushdb()
     
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in Redis with random key and return the key"""
         key = str(uuid.uuid4())
